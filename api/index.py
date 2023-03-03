6175cd052002a6f96bdcf9f6feed609868a7cf31
Jack Bauerle <jbauerle@wayfair.com>
2023-03-02 23:08:29 -0500

    fixing path

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='infinitron-react/build')
+app = Flask(__name__, static_folder='../infinitron-react/build')
 
 @app.route('/')
 def home():
