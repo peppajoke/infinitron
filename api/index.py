f980eeddcd71d08a5d265671cb671971925c10a8
Jack Bauerle <jbauerle@wayfair.com>
2023-03-02 23:32:33 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,7 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='../infinitron-react/build')
 
+# Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
@@ -11,10 +12,6 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-
-@app.route('/about')
-def about():
-    return 'About'
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
